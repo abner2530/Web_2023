7ea93658a8d3cbfefc92b1a39d3287d725f80a5e
Abner Monteiro <abner_monteiro_@hotmail.com>
2023-06-25 21:51:12 -0300

    up

---
+++ introducao/src/components/14MUI/02CRUD/MainPage.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import MyMenuV1 from "./MyMenuV1"
+import { Container } from "@mui/material"
+//páginas do Professor
+import CadastrarProfessor from "./professor/Cadastrar"
+import ListarProfessor from "./professor/Listar"
+import EditarProfessor from "./professor/Editar"
+
+//páginas do Aluno
+import CadastrarAluno from "./aluno/Cadastrar"
+import ListarAluno from "./aluno/Listar"
+import EditarAluno from "./aluno/Editar"
+
+const MainPage = () => {
+    return (
+        <BrowserRouter>
+            <MyMenuV1 />
+            <Container sx={{mt:4}}>
+                <Routes>
+                    <Route path="cadastrarProfessor" element={<CadastrarProfessor />}/>
+                    <Route path="listarProfessor" element={<ListarProfessor />}/>
+                    <Route path="editarProfessor/:id" element={<EditarProfessor />}/>
+                    <Route path="cadastrarAluno" element={<CadastrarAluno />}/>
+                    <Route path="listarAluno" element={<ListarAluno />}/>
+                    <Route path="editarAluno/:id" element={<EditarAluno />}/>
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    )
+}
+
+export default MainPage
